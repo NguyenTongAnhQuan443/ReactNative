4b09dd1674f7028b0802f6971304d2e0c698842c
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2024-08-19 13:56:58 +0700

    cau 5

--- ReactNative_Week1/week1.js
+++ ReactNative_Week1/week1.js
@@ -11,6 +11,23 @@
 // }
 
 // Cau 3 
-for(let i = 1 ; i < 11; i++){
-    console.log(`7 * ${i} = ${7 * i}`)
+// for(let i = 1 ; i < 11; i++){
+//     console.log(`7 * ${i} = ${7 * i}`)
+// }
+
+// Cau 4
+// for (let i = 1; i < 11; i++) {
+//     for (let j = 1; j < 11; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`)
+//     }
+// }
+
+// Cau 5
+const sum = sum1To10 = () => {
+    let sum = 0;
+    for (let i = 1; i < 11; i++) {
+        sum += i;
+    }
+    console.log("🚀 ~ sum:", sum)
 }
+sum()
