6b2ae6b673e4f0e9141f3a8be37f17f2c89ec964
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2024-08-19 13:44:12 +0700

    cau 3

--- ReactNative_Week1/week1.js
+++ ReactNative_Week1/week1.js
@@ -4,8 +4,13 @@
 // }
 
 // Cau 2
-for(let i = 0 ; i < 100; i++){
-    if(i % 2 != 0){
-        console.log(i)
-    }
+// for(let i = 0 ; i < 100; i++){
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+// }
+
+// Cau 3 
+for(let i = 1 ; i < 11; i++){
+    console.log(`7 * ${i} = ${7 * i}`)
 }
