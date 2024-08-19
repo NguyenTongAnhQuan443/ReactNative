cfd139f12d9c3c526f3ca4da2b6dd0c4ca61b891
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2024-08-19 13:40:24 +0700

    cau 2

--- ReactNative_Week1/week1.js
+++ ReactNative_Week1/week1.js
@@ -1,4 +1,11 @@
 // Cau 1
-for(let i = 0 ; i < 10; i++){
-    console.log(i)
+// for(let i = 0 ; i < 10; i++){
+//     console.log(i)
+// }
+
+// Cau 2
+for(let i = 0 ; i < 100; i++){
+    if(i % 2 != 0){
+        console.log(i)
+    }
 }
