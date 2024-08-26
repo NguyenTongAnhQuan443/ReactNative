c2eecf38dffc3496ffed4c85b9d04ad698020f44
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2024-08-26 14:39:19 +0700

    cau 2_tuan2

--- ReactNative_Week2/index.js
+++ ReactNative_Week2/index.js
@@ -23,16 +23,72 @@ function compareBMI(bmiMark, bmiJohn){
 }
 
 // callback
-calculateBMI(markMass1, markHeight1, function(markBMI1){
+const f1 = calculateBMI(markMass1, markHeight1, function(markBMI1){
     calculateBMI(johnMass1, johnHeight1, function(johnBMI1){
         let compare = compareBMI(markBMI1, johnBMI1);
         console.log("Data 1: Mark BMI is height: ", compare)
     });
 });
 
+const f2 = calculateBMI(markMass2, markHeight2, function(markBMI2){
+    calculateBMI(johnMass2, johnHeight2, function(johnBMI2){
+        let compare = compareBMI(markBMI2, johnBMI2);
+        console.log("Data 2: Mark BMI is height: ", compare)
+    });
+});
+
+
+// Coding Challenge #2
+calculateBMI(markMass1, markHeight1, function(markBMI1){
+    calculateBMI(johnMass1, johnHeight1, function(johnBMI1){
+        let compare = compareBMI(markBMI1, johnBMI1);
+
+        if(compare){
+            console.log(`Data 1: Mark's BMI is higher than John's!`)
+        }else{
+            console.log(`Data 1: John's BMI is higher than Mark's!b`)
+        }
+    })
+})
+
 calculateBMI(markMass2, markHeight2, function(markBMI2){
     calculateBMI(johnMass2, johnHeight2, function(johnBMI2){
         let compare = compareBMI(markBMI2, johnBMI2);
-        console.log("Data 1: Mark BMI is height: ", compare)
+
+        if(compare){
+            console.log(`Data 2: Mark's BMI is higher than John's!`)
+        }else{
+            console.log(`Data 2: John's BMI is higher than Mark's!b`)
+        }
     });
 });
+
+function bmiOnePeople(functionBMI){
+    return functionBMI;
+}
+
+calculateBMI(markMass1, markHeight1, function(markMass1, markHeight1 ){
+    calculateBMI(johnMass1, johnHeight1, function(johnMass1, johnHeight1){
+        let bmiMark = bmiOnePeople(markMass1,markHeight1);
+        let bmiJohn = bmiOnePeople(johnMass1, johnHeight1);
+
+        if(bmiMark > bmiJohn){
+            console.log(`Data 1: Mark's BMI(${bmiMark}) is higher than John's(${bmiJohn})!`)
+        }else{
+            console.log(`Data 1: John's BMI(${bmiJohn}) is higher than Mark's(${bmiMark})!`)
+        }
+    });
+});
+
+calculateBMI(markMass2, markHeight2, function(markMass2, markHeight2){
+    calculateBMI(johnMass2, johnHeight2, function(johnMass2, johnHeight2){
+        let bmiMark = bmiOnePeople(markMass2, markHeight2);
+        let bmiJohn = bmiOnePeople(johnMass2, johnHeight2);
+
+        if(bmiMark > bmiJohn){
+            console.log(`Data 2: Mark's BMI(${bmiMark}) is higher than John's(${bmiJohn})!`)
+        }else{
+            console.log(`Data 2: John's BMI(${bmiJohn}) is higher than Mark's(${bmiMark})!`)
+        }
+    })
+})
