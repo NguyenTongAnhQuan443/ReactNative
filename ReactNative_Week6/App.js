1d87ad33c9a90f7e2b348f292af67fa87bacdf99
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2024-09-30 15:53:55 +0700

    Update week 6 - screen 1 - 2 (Done)

--- ReactNative_Week6/App.js
+++ ReactNative_Week6/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Screen01 from './componets/Screen01';
+import Screen02 from './componets/Screen02';
 export default function App() {
   return (
     // <View style={styles.container}>
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
-    <Screen01/>
+    // <Screen01/>
+    <Screen02/>
   );
 }
 
---
+++ ReactNative_Week6/README.md
@@ -0,0 +1,4 @@
+# Screen 01 
+![alt text](assets/minhChung/Screen01.jpg)
+# Screen 02
+![alt text](assets/minhChung/Screen02.png)
